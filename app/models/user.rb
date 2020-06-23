a1bc22d79df7841cbd22ecb8087ed1ad06c4cc89
Valentine Ecrepont <ecrepontvalentine@hotmail.fr>
2020-06-23 12:40:28 +0200

    user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :reports
+  has_many :reviews
+  has_many :favorites, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :first_name, :last_name, :address, presence: true
+
+
 end
