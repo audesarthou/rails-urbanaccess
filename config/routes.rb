8e286384b200eb840966e35e9e67da82f4610f18
quentin lajoie <qnlajoie@gmail.com>
2020-06-23 15:15:27 +0200

    Routes+Controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :establishments, only: [:index, :show, :new, :create, :edit, :update] do
+    resources :reviews, only: [:create]
+    resources :favorites, only: [:create]
+  end
+  resources :favorites, only: [:index]
+
+  resources :travel_tips, only: [:index]
+
+  resources :report, only: [:create, :index]
 end
