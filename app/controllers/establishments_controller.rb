e37f4b5bcc5356580cf10097016d22e5ab6f7fb3
quentin lajoie <qnlajoie@gmail.com>
2020-06-24 11:42:31 +0200

    establishment new+create

--- app/controllers/establishments_controller.rb
+++ app/controllers/establishments_controller.rb
@@ -1,7 +1,21 @@
 class EstablishmentsController < ApplicationController
+  def new
+    @establishment = Establishment.new
+  end
+
+  def create
+    @establishment = Establishment.new(establishment_params)
+    if @establishment.save
+      redirect_to establishment_path(@establishment)
+    else
+      render :new
+    end
+  end
+
   def edit
     @establishment = Establishment.find(params[:id])
   end
+
   def update
     @establishment = Establishment.find(params[:id])
     if @establishment.update(establishment_params)
