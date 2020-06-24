8059859524e59c6cf7eacbca20fdbdeeef67bad3
aude sarthou <audesarthou33@gmail.com>
2020-06-24 17:49:59 +0200

    cloudinary config

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_06_24_091059) do
+ActiveRecord::Schema.define(version: 2020_06_24_102058) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
