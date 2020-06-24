adc45deb349f7f3dd3ae5e0047932cea0783f3a5
Valentine Ecrepont <ecrepontvalentine@hotmail.fr>
2020-06-24 12:28:41 +0200

    add gem geocoding

--- app/models/establishment.rb
+++ app/models/establishment.rb
@@ -6,4 +6,6 @@ class Establishment < ApplicationRecord
   validates :name, uniqueness: { scope: :address }
   validates :category, inclusion: { in: %w(restaurant bar cinema theatre logement:location logement:achat),
     message: "%{value} is not a valid category" }
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
