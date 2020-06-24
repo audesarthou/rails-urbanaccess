e33d43fdccb142df9f5559e8b18a7c18073735ec
Valentine Ecrepont <ecrepontvalentine@hotmail.fr>
2020-06-24 14:43:30 +0200

    seed user and establishments

--- app/models/establishment.rb
+++ app/models/establishment.rb
@@ -4,7 +4,7 @@ class Establishment < ApplicationRecord
   has_many :accessibilities, dependent: :destroy
   validates :name, :address, :description, :category, presence: true
   validates :name, uniqueness: { scope: :address }
-  validates :category, inclusion: { in: %w(restaurant bar cinema theatre logement:location logement:achat),
+  validates :category, inclusion: { in: %w(Restaurant Bar Cinema Theatre Logement:location logement:achat),
     message: "%{value} is not a valid category" }
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
