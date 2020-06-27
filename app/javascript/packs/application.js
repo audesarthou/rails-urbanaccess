1c281d8faa7a51ab16405a92a7ea1a2d1377fd84
Valentine Ecrepont <ecrepontvalentine@hotmail.fr>
2020-06-27 23:52:49 +0200

    add and custom  select2 to home page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -30,10 +30,11 @@ import "bootstrap";
 // import { initSelect2 } from '../components/init_select2';
 import { initMapbox } from '../plugins/init_mapbox';
 import { initMapboxShow } from '../plugins/init_mapbox_show';
+import { initSelect2 } from '../plugins/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
-  // initSelect2();
+  initSelect2();
   initMapbox();
   initMapboxShow();
 });
